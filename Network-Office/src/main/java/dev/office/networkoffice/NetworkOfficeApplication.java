1c9166d811a1e5722713e08569358e4437eb82cf
Yehyeok Bang <qkddpgur318@gmail.com>
2024-08-25 19:07:54 +0900

    [#1] init: 프로젝트 생성
    - Java 17 버전을 사용합니다.
    - Web, DB 등 필요 의존성을 추가합니다.

---
+++ Network-Office/src/main/java/dev/office/networkoffice/NetworkOfficeApplication.java
@@ -0,0 +1,13 @@
+package dev.office.networkoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkOfficeApplication.class, args);
+    }
+
+}
---
+++ Network-Office/src/test/java/dev/office/networkoffice/NetworkOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.office.networkoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
