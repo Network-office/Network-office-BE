bb948afbfa1c3014a3371607426ff19a23c372bf
Yehyeok Bang <qkddpgur318@gmail.com>
2024-09-03 01:33:46 +0900

    [#8] feat: 공통 예외 핸들링 템플릿 추가

---
+++ Network-Office/src/main/java/dev/office/networkoffice/global/exception/dto/ExceptionResponse.java
@@ -0,0 +1,8 @@
+package dev.office.networkoffice.global.exception.dto;
+
+public record ExceptionResponse(String message) {
+
+    public static ExceptionResponse of(String message) {
+        return new ExceptionResponse(message);
+    }
+}
