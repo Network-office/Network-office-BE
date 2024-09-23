047e2a3c46af6e8057a22c5b05ee78a7f2a9ef2a
soohyun <sss4920@naver.com>
2024-09-23 17:18:37 +0900

    [#6] refactor: Blocked User 엔티티 중간 엔티티 개념으로 변경

--- Network-Office/src/main/java/dev/office/networkoffice/gathering/entity/BlockedUser.java
+++ Network-Office/src/main/java/dev/office/networkoffice/gathering/entity/BlockedUser.java
@@ -1,11 +1,17 @@
 package dev.office.networkoffice.gathering.entity;
 
+import dev.office.networkoffice.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,12 +25,28 @@ import lombok.NoArgsConstructor;
 @Table(name = "blocked_users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BlockedUser {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "blocked_user_id")
 	private Long id;
-	private Long userId;
-	private Long gatheringId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "gathering_id")
+	private Gathering gathering;
+
 	private String reason;
+
+	@Builder
+	public BlockedUser(User user, Gathering gathering, String reason) {
+		this.user = user;
+		this.gathering = gathering;
+		this.reason = reason;
+	}
 
 	public void writeReasonsForRejection(String reason){
 		this.reason = reason;
